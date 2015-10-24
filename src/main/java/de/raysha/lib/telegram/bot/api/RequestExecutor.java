c69ee9706338785ae1d3030776fdebc90999042f
Igor Artamonov <igor@artamonov.ru>
2015-10-24 13:08:04 +0600

    Javadoc for RequestExecutor

--- src/main/java/de/raysha/lib/telegram/bot/api/RequestExecutor.java
+++ src/main/java/de/raysha/lib/telegram/bot/api/RequestExecutor.java
@@ -10,10 +10,36 @@ import java.util.Map;
  */
 public interface RequestExecutor {
 
+    /**
+     * Make GET request to the API
+     *
+     * @param action API method name (/bot%TOKEN%/%ACTION%)
+     * @param parameters query parameters
+     * @return value of 'result' field
+     * @throws BotException on internal exception (IO, invalid response, etc
+     */
     String get(String action, Map<String, Object> parameters) throws BotException;
 
+    /**
+     * Make POST request to the API
+     *
+     * @param action API method name (/bot%TOKEN%/%ACTION%)
+     * @param parameters query parameters
+     * @return value of 'result' field
+     * @throws BotException on internal exception (IO, invalid response, etc
+     */
     String post(String action, Map<String, Object> parameters) throws BotException;
 
+    /**
+     * Make POST request to the API, with a file
+     *
+     * @param action API method name (/bot%TOKEN%/%ACTION%)
+     * @param parameters query parameters
+     * @param fileName a parameter name for the file ('photo', 'sticker', etc)
+     * @param file file to upload
+     * @return value of 'result' field
+     * @throws BotException on internal exception (IO, invalid response, etc
+     */
     String post(String action, Map<String, Object> parameters, String fileName, File file) throws BotException;
 
 }
